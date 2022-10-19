fdbb6d594ed0f1f249bfcf2fadf47f141983fbd8
jadefogarty <73829980+jadefogarty@users.noreply.github.com>
2022-10-19 15:42:42 +0100

    Update jade_js.js

--- js/jade_js.js
+++ js/jade_js.js
@@ -1,14 +1,6 @@
 //console.log("js file connected");
-
 const api_key = "cjzAWKyCfClMXNLBEVqeZTh21wcXiNefpJw6glYe"
 
-
-//console.log(dateInput);
-function selectDate(){
-    
-    
-    
-}
 
 async function loadInfo(){
 
@@ -19,18 +11,24 @@ async function loadInfo(){
     var mainURL = basicURL + "&date=" +dateInput
     //console.log(basicURL);
     console.log("url is:" +mainURL);
-    document.getElementById("link").innerHTML= "Info from: https://" +mainURL
+    //document.getElementById("link").innerHTML= "Info from: https://" +mainURL
 
     var result= "";
     var response = await fetch("https://"+mainURL);
     var data = await response.json();
     console.log(data)
 
-    result+="<div class=\"container-fluid\"><div class=\"row\"><h2> " + data["title"] + " </h2></div></div>";
-    result+="<div class=\"container-fluid\"> <div class=\"row\"><img src=\"" + data["url"] + "\" alt=\"image\"></div></div>";
-    result+="<div class=\"container-flui\"><div class=\"container-fluid\"><p>" + data["explanation"] + "</p></div></div>";
+    //display title
+    result+="<div class=\"container-fluid\"><div class=\"row\ dataTitle\"><h2> " + data["title"] + " </h2></div></div>";
+    //result+="<br />"
+    //display image
+    result+="<div class=\"container-fluid\"> <div class=\"row\"><img class=\"myImg\" src=\"" + data["url"] + "\" alt=\"image\"></div></div>";
+    result+="<br />"
+    //display info
+    result+="<div class=\"container-fluid\"><div class=\"row\"><h3 class=\"infoTitle\"> Information </h3></div> <div class=\"row\"><p>" + data["explanation"] + "</p></div></div>";
+    result+="<br />"
 
-    console.log(result);
+    //console.log(result);
     document.getElementById("content").innerHTML = result;
     
 
